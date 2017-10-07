7c0e7cf24f72641d93db793e47f32a33998cefa0
glnosg <glina.osg@gmail.com>
2017-10-07 11:46:24 +0200

    Add MusicPattern fragment, and ControlPanel fragment

--- app/src/main/java/com/glnosg/beatboxer/ControlPanelFragment.java
+++ app/src/main/java/com/glnosg/beatboxer/ControlPanelFragment.java
@@ -1,8 +1,21 @@
 package com.glnosg.beatboxer;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by pawel on 07.10.17.
  */
 
-public class ControlPanelFragment {
+public class ControlPanelFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_control_panel, container, false);
+        return view;
+    }
 }
--- app/src/main/java/com/glnosg/beatboxer/MusicPatternFragment.java
+++ app/src/main/java/com/glnosg/beatboxer/MusicPatternFragment.java
@@ -1,8 +1,21 @@
 package com.glnosg.beatboxer;
 
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
 /**
  * Created by pawel on 07.10.17.
  */
 
-public class MusicPatternFragment {
+public class MusicPatternFragment extends Fragment{
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_music_pattern, container, false);
+        return view;
+    }
 }

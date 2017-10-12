f3349a73a5594e5c09690b718bdabca645a4f536
glnosg <glina.osg@gmail.com>
2017-10-12 19:29:56 +0200

    Create OnButtonListener in ControlPanelFragment

--- app/src/main/java/com/glnosg/beatboxer/ControlPanelFragment.java
+++ app/src/main/java/com/glnosg/beatboxer/ControlPanelFragment.java
@@ -1,6 +1,7 @@
 package com.glnosg.beatboxer;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,10 +13,28 @@ import android.view.ViewGroup;
 
 public class ControlPanelFragment extends Fragment {
 
+    private OnButtonClickListener mCallback;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_control_panel, container, false);
         return view;
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+
+        try {
+            mCallback = (OnButtonClickListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString()
+                + " must implement OnButtonClickListener");
+        }
+    }
+
+    public interface OnButtonClickListener {
+        public void onOptionSelected();
     }
 }
--- app/src/main/java/com/glnosg/beatboxer/MainActivity.java
+++ app/src/main/java/com/glnosg/beatboxer/MainActivity.java
@@ -2,12 +2,21 @@ package com.glnosg.beatboxer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import static android.os.Build.VERSION_CODES.M;
+
+public class MainActivity extends AppCompatActivity
+    implements ControlPanelFragment.OnButtonClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onOptionSelected() {
+
     }
 }
